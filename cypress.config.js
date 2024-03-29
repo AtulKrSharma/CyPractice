25eda52f9f44cc288d2b339dff4e2a071f68ca86
Atul Sharma <atul.sharma@fcc-fac.ca>
2024-03-29 09:41:19 -0400

    spec pattern added

--- cypress.config.js
+++ cypress.config.js
@@ -10,9 +10,17 @@ module.exports = defineConfig({
     saveAllAttempts: true,
     videoOnFailOnly: true,
   },
+  watchForFileChanges: true,
+  retries: { runMode: 1, openMode: 1 },
+  trashAssetsBeforeRuns: true,
+  video: true,
+  waitForAnimations: true,
+  specPattern: 'cypress/e2e/basic/**/*.cy.{js,jsx,ts,tsx}',
+
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
+    experimentalRunAllSpecs: true,
   },
 });
